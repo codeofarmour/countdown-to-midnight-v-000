69b37969bf6e93597295e87ef682fe294ea0f733
Terri Riggle <codeofarmour@outlook.com>
2017-10-04 18:11:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,14 @@ def countdown(countdown_to_midnight)
   puts "#{countdown_to_midnight} SECOND(S)!"
   countdown_to_midnight -= 1
 end
-
 "HAPPY NEW YEAR!"
-
 end
 
-def countdown
+def countdown_with_sleep(countdown_to_midnight)
+  while countdown_to_midnight > 0
+    puts "#{countdown_to_midnight} SECOND(S)!"
+    sleep(1)
+    countdown_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
